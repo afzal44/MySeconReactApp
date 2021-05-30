528277872ee51ce08271e7567d4422fca4efb042
afzal44 <afjal.ahamad44@gmail.com>
2021-05-30 17:14:54 +0530

    My First Commit to todo APP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React,{ useEffect, useState } from 'react';
+import Header from './MyComponents/Header';
+import {Todos} from './MyComponents/Todos';
+import {Footer} from './MyComponents/Footer';
+import {AddTodo} from './MyComponents/AddTodo';
+import {About} from './MyComponents/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  let initTodo;
+  if(localStorage.getItem("todos")===null){
+    initTodo = [];
+      }
+    else{
+      initTodo = JSON.parse(localStorage.getItem("todos"));
+    }
+  const addTodo = (title,desc)=>{
+    console.log("i am adding these todo ",title,desc)
+    let sno;
+    if(todos.length===0){
+      sno = 1
+    }
+    else{
+    sno = todos[todos.length-1].sno +1;}
+    const mytodo = {
+      sno: sno,
+      title: title,
+      desc: desc
+    }
+    setTodos([...todos,mytodo])
+    console.log(mytodo)
+    
+  }
+  const onDelete = (todo) => {
+    // console.log("I AM OnDelete of todo",todo)
+    setTodos(todos.filter((e)=>{
+      return e!==todo;
+    }));
+    // localStorage.setItem("todos",JSON.stringify(todos))
+  }
+const [todos,setTodos]=useState([initTodo]);
+useEffect(() => {
+  localStorage.setItem("todos",JSON.stringify(todos))
+}, [todos])
+return (
+    <>
+    <Router>
+     <Header title="My Todos List" searchBar={true}/>
+     <Switch>
+          <Route exact path="/" render={()=>{
+            return( <>
+            <AddTodo addTodo={addTodo}></AddTodo>
+            <Todos todos={todos} onDelete={onDelete}/>
+            </>)
+          }}>
+
+            </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+         
+      </Switch>
+          <Footer/>
+     </Router>
+    </>
   );
 }
 
---
+++ src/MyComponents/AddTodo.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useState } from 'react';
+
+export const AddTodo = (props) => {
+    const [title, setTitle] = useState("");
+    const [desc,setDesc] = useState("");
+
+    const submit = (e)=>{
+        e.preventDefault();
+        if(!title || !desc)
+        {
+            alert("Title or Description cannnot be blank")
+            
+        }
+        else{
+        props.addTodo(title,desc);
+        setTitle("")
+        setDesc("")
+        }
+    }
+    return (
+        <div className="container my-3">
+            <h3>Add a Todo</h3>
+            <form onSubmit={submit}>
+                <div className="mb-3">
+                    <label htmlFor="title" className="form-label">Todo Title</label>
+                    <input type="text" className="form-control" value={title} onChange={(e)=>{setTitle(e.target.value)}} 
+                    id="title" aria-describedby="emailHelp"/>
+                        {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+                </div>
+                    <div className="mb-3">
+                        <label htmlFor="desc" className="form-label">Todo Description</label>
+                        <input type="text" value={desc} onChange={(e)=>{setDesc(e.target.value)}} className="form-control" id="desc"/>
+                    </div>
+                        {/* <div className="mb-3 form-check">
+                            <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                                <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div> */}
+                            <button type="submit" className="btn btn-sm btn-success">Add Todo</button>
+            </form>
+        </div>
+    )
+}
